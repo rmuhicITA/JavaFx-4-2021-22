05d1aa8fb781cc0c21b1d6c96effb5e9f729ca26
rijad.muhic <rijad.muhic@fba.ba>
2022-04-13 16:49:30 +0200

    Java fx

--- src/main/java/com/example/sceneproject/SceneFocus.java
+++ src/main/java/com/example/sceneproject/SceneFocus.java
@@ -5,10 +5,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -18,13 +22,80 @@ public class SceneFocus extends Application {
         launch(args);
     }
 
-    private Stage stage;
+    //Login form
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("JavaFx GridPane");
+        GridPane formGridPane = new GridPane();
+
+        //spoljasni i unutarnji razmak
+        formGridPane.setPadding(new Insets(10, 10, 10, 10));
+        GridPane.setMargin(formGridPane, new Insets(20, 20, 20, 20));
+        formGridPane.setVgap(8);
+        formGridPane.setHgap(10);
+
+        //Username ili korisnički nalog
+        Label usernameLabel = new Label("Username");
+        GridPane.setConstraints(usernameLabel, 0, 0);
+        TextField usernameTextField = new TextField();
+        usernameTextField.setPromptText("Enter username....");
+        GridPane.setConstraints(usernameTextField, 1, 0);
+
+        //Password ili lozinka
+        Label passwordLabel = new Label("Password");
+        GridPane.setConstraints(passwordLabel, 0, 1);
+        PasswordField passwordTextField = new PasswordField();
+        passwordTextField.setPromptText("Enter password..");
+        GridPane.setConstraints(passwordTextField, 1, 1);
+
+        //LOGIN button
+        Button loginButton = new Button("Sign in");
+        GridPane.setConstraints(loginButton, 1, 2);
+        ObservableList<Node> children = formGridPane.getChildren();
+        children.addAll(usernameLabel, usernameTextField, passwordLabel, passwordTextField, loginButton);
+        Scene scene = new Scene(formGridPane, 300, 250);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+    /*@Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Slaganje kontejnera unutar kontejnera");
+        HBox topMenuBox = new HBox();
+        Button fileButton = new Button("File");
+        Button editButton = new Button("Edit");
+        Button viewButton = new Button("View");
+        topMenuBox.getChildren().addAll(fileButton, editButton, viewButton);
+        VBox leftMenuBox =new VBox();
+        Button fileButton1 = new Button("File");
+        Button editButton1 = new Button("Edit");
+        Button viewButton1 = new Button("View");
+        leftMenuBox.getChildren().addAll(fileButton1, editButton1, viewButton1);
+        BorderPane borderPane = new BorderPane();
+        //top left center right bottom
+        borderPane.setTop(topMenuBox);
+        borderPane.setLeft(leftMenuBox);
+        Scene scene = new Scene(borderPane, 300, 250);
+        stage.setScene(scene);
+        stage.show();
+    }*/
+
+
+
+    /*private Stage stage;
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
         stage.setTitle("Closing of window");
-        stage.setOnCloseRequest(this::closeProgram);
+        //stage.setOnCloseRequest(this::closeProgram);
+        stage.setOnCloseRequest(e->{
+            //Java needs to believe us during closing app
+            e.consume();
+            closeProgram(e);
+        });
         Button button = new Button("Close app");
         //button.setOnAction(e->closeProgram());
         //EventHandler<ActionEvent> handler = this::closeProgram;
@@ -49,7 +120,7 @@ public class SceneFocus extends Application {
             System.out.println("File isn't saved...");
         }
 
-    }
+    }*/
 
 
 
